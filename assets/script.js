3936929d0e2569d9368a0730bae84caa60c18412
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-05-29 17:33:15 +1000

    Fix js source in HTML, add JS function to generate password text.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,155 @@
-// Assignment code here
+
+
+// Array of lowercase characters 
+var lowerCaseCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters 
+var upperCaseCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Array of numeric characters 
+var numericCharacters = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9'
+ ];
+
+// Array of special chartacters
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+function generatePassword() {
+  // Ask user for their password length.
+  var userChoiceLength = window.prompt("How many characters would you like your password to contain?");
+
+  // If password length is less than 8 characters or more than 128 characters, restart the application.
+  if (userChoiceLength < 8) {
+    alert ("Password length must be at least 8 characters.");
+    return;
+  }
+
+  if (userChoiceLength > 128) {
+    alert ("Password length must be less than 129 characters.");
+    return;
+  } 
+
+  // Ask user for their password criteria.
+  var userChoiceLowerCase = window.confirm("Click OK if you would you like your password to include lowercase characters.");
+  var userChoiceUpperCase = window.confirm("Click OK if you would you like your password to include uppercase characters.");
+  var userChoiceNumeric = window.confirm("Click OK if you would you like your password to include numeric characters.");
+  var userChoiceSpecial = window.confirm("Click OK if you would you like your password to include special characters.");
+
+  // At least one password criteria must be selected. If no password criteria are selected, restart the application.
+  if (!userChoiceLowerCase && !userChoiceUpperCase && !userChoiceNumeric && !userChoiceSpecial) {
+    alert("At least one password criteria needs to be selected.");
+    return;
+  }
+
+  // Combine selected characters into one array.
+  var userChoiceCombined = [
+    ...(userChoiceLowerCase ? lowerCaseCharacters : []),
+    ...(userChoiceUpperCase ? upperCaseCharacters : []),
+    ...(userChoiceNumeric ? numericCharacters : []),
+    ...(userChoiceSpecial ? specialCharacters : []),
+  ]
+
+  /* 
+  Randomly select a character from selected arrays.
+  Add the randomly selected character to the password.
+  Repeat this for the number of password characters specified by the user. 
+  */
+    for (var i = 0, text = ""; i < userChoiceLength; i++) {
+      var randomCharacterIndex = Math.floor(Math.random() * userChoiceCombined.length);
+      var randomCharacter = userChoiceCombined[randomCharacterIndex];
+      text += randomCharacter
+    }
+
+  console.log(text)
+
+}
 
 
 // Get references to the #generate element
