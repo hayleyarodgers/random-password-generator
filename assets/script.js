202bb07cce3ebf43ad61bdf2545b11af504b7b1e
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-05-29 18:29:36 +1000

    Show placeholder if password is undefined

--- assets/script.js
+++ assets/script.js
@@ -145,7 +145,7 @@ function generatePassword() {
       var randomCharacter = userChoiceCombined[randomCharacterIndex];
       text += randomCharacter
     }
-
+  
   return text
 
 }
@@ -157,9 +157,11 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  if (typeof password === 'undefined') {
+    passwordText.value = "";
+  } else {
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
